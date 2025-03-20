8498b25372f2bd8ec91942db79c5201e78632bf2
Kevin Christman Lumban Tobing <KEVINTOBINGGAMING@GMAIL.COM>
2025-03-20 13:43:15 +0700

    Memberikan click active

--- frontend/src/components/Home/ToLogin.jsx
+++ frontend/src/components/Home/ToLogin.jsx
@@ -19,9 +19,6 @@ const ToLogin = () => {
       <motion.button
         className="mt-5 bg-black text-white px-6 py-2 rounded-full text-sm sm:text-base font-medium transition duration-300 hover:bg-gray-800 active:scale-90"
         whileTap={{ scale: 0.8, transition: { duration: 0.0 } }}
-        className="mt-5 bg-black text-white px-6 py-2 rounded-full text-sm sm:text-base font-medium hover:bg-gray-800 transition duration-300 active:scale-90"
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
       >
         Login
       </motion.button>
@@ -30,3 +27,4 @@ const ToLogin = () => {
 };
 
 export default ToLogin;
+
