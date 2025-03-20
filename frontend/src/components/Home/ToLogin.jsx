2d47ed92554937b0d79a8ec5bebcfa8d09051701
unknown <kevintobinggaming@gmail.com>
2025-03-21 00:13:09 +0700

    Menambahkan halaman login dan register

--- frontend/src/components/Home/ToLogin.jsx
+++ frontend/src/components/Home/ToLogin.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const ToLogin = () => {
   return (
     <div className="flex flex-col items-center justify-center py-20 px-4">
@@ -7,12 +9,10 @@ const ToLogin = () => {
       <p className="text-sm sm:text-base text-gray-600 text-center mt-2">
         Log in untuk membuat akun
       </p>
-
-      <button
-        className="mt-5 bg-black text-white px-6 py-2 rounded-full text-sm sm:text-base font-medium transition duration-300 hover:bg-gray-800 active:scale-90"
-      >
+      
+      <Link to="/login" className="mt-5 bg-black text-white px-6 py-2 rounded-full text-sm sm:text-base font-medium transition duration-300 hover:bg-gray-800 active:scale-90">
         Login
-      </button>
+      </Link>
     </div>
   );
 };
